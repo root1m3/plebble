607a90053faf7f17cc87c0e9b438bbf0e60d3e56
Codeindeed <favouralex084@gmail.com>
2022-06-26 13:36:12 +0100

    some final changes before pushing

--- core1/brands/plebble/front/animate.js
+++ core1/brands/plebble/front/animate.js
@@ -1,10 +1,10 @@
 AOS.init({
   // Global settings:
-  disable: true, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
-  delay: 1000, // values from 0 to 3000, with step 50ms
+  delay: 5000, // values from 0 to 3000, with step 50ms
   duration: 200, // values from 0 to 3000, with step 50ms
   easing: "ease", // default easing for AOS animations
   once: true, // whether animation should happen only once - while scrolling down
